a940bf37f8dff634797809b690a20a74de7a6cb5
mattia46old <mattiacolalongo82@gmail.com>
2024-12-31 13:01:56 +0100

    retouched main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from pizza import Pizza, PizzaAbundant
 
 mypizza = Pizza(15, 70)
 #mypizza.all_the_ingredients()
+print(f"\npizza for {mypizza.npeople} people and {mypizza.hydr}% hydration")
 mypizza.all_table()
 
 
 morepizza = PizzaAbundant(15, 70)
 #morepizza.all_the_ingredients()
+print(f"\nfull belly pizza for {mypizza.npeople} same hydration")
 morepizza.all_table()
